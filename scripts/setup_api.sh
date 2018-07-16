5625441b82465c28a358305db1e083446bfccacc
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-07-16 17:11:44 +0200

    travis test

--- scripts/setup_api.sh
+++ scripts/setup_api.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-echo hello!
+docker service create --name registry --publish published=5000,target=5000 registry:2
+docker build -t 127.0.0.1:5000/docks-ui:local .
+docker push 127.0.0.1:5000/docks-ui:local
+docker stack deploy -c docker-compose-api.yml docks-api
