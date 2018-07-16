3aaa53e7b238feae3afe844b2382d361958afa8e
CDuPlooy <armandconnor.duplooy@gmail.com>
2018-07-16 17:50:13 +0200

    Testing

--- src/app/_services/auth/auth.service.spec.ts
+++ src/app/_services/auth/auth.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientModule } from '@angular/common/http';
 
-import { AuthService } from './auth.service';
+import {AuthError, AuthService} from './auth.service';
 import { ConfigurationService } from '../configuration/configuration.service';
 import { TokenStorage } from '../../_classes';
 
@@ -17,4 +17,21 @@ describe('AuthService', () => {
   it('should be created', inject([AuthService], (service: AuthService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('should have all functions', inject([AuthService], (service: AuthService) => {
+    expect(service.getToken).toBeTruthy();
+    expect(service.isLoggedIn).toBeTruthy();
+    expect(service.logout).toBeTruthy();
+  }));
+
+    it('it should authenticate', inject([AuthService, ConfigurationService], (service: AuthService, config: ConfigurationService) => {
+        service.getToken('admin', 'admin').subscribe(authError => {
+          let got_response = false;
+          if(authError === AuthError.AUTH_OK) {
+            got_response = true;
+          }
+          expect(got_response).toEqual(true);
+        });
+    }));
 });
+
